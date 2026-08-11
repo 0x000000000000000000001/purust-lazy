5720fcc8e61dcd581c9d6ced148d834027c512eb
0x1 <kevin.francart3@gmail.com>
2026-08-11 12:53:18 +0200

    misc

--- src/Data/Lazy.rs
+++ src/Data/Lazy.rs
@@ -1,1 +1,7 @@
-pub type Lazy = crate::UnknownType;
+
+pub fn Data_Lazy_force() -> crate::UnknownType { crate::UnknownType::new(0) }
+pub fn Data_Lazy_defer() -> crate::UnknownType { crate::UnknownType::new(0) }
+
+pub fn Data_Lazy_force() -> crate::UnknownType { crate::UnknownType::new(0) }
+
+pub fn Data_Lazy_defer() -> crate::UnknownType { crate::UnknownType::new(0) }
